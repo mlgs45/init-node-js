adae9e8a4706f57cbfe169cff7539c1c2448f889
Mathieu LGS <mathieu.langlois@voxm.live>
2022-09-25 14:23:51 +0200

    Update index with const for cowsay

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const informationCampus = require('./information');
-var cowsay = require("cowsay");
+const cowsay = require("cowsay");
 
 console.log(cowsay.say({
     text : `Hi, my name is ${informationCampus.firstName} and I'm an student of ${informationCampus.campus}`,
